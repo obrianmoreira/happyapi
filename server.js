951a5c500f02adfa844a7bce1e40d20ea474bf01
obrianmoreira <brian.agnilo@gmail.com>
2022-09-29 11:57:15 +0200

    first version

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = new express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb+srv://obrianmoreira:KaplerB@cluster0.r5ewwpf.mongodb.net/happyDB?retryWrites=true&w=majority', {useNewUrlParser: true}, {useUnifiedTopology: true})
+//mongodb+srv://obrianmoreira:KaplerB@cluster0.r5ewwpf.mongodb.net/?retryWrites=true&w=majority
+
+// Create a data Schema
+const leadSchema = {
+    name: String,
+    email: String,
+    phone: String
+}
+
+const Lead = mongoose.model('Lead', leadSchema);
+
+//app.get("/", (req, res)=>{
+    //res.sendFile(__dirname + "/src/index.html");
+    //res.render(__dirname + "/views/pages/index.ejs",{});
+//})
+
+app.post("/leads", (req, res) =>{
+    console.log('Chegou');
+    let newLead = new Lead({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone
+    })
+    newLead.save();
+    res.redirect("http://happyapp-cleaning.vercel.app");
+})
